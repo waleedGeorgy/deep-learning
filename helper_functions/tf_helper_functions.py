bda18a2ffb8dc64a9ba46831cc30142c5e29c2b1
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2024-10-18 06:46:38 -0400

    Added TensorFlow helper functions file

---
+++ helper_functions/tf_helper_functions.py
@@ -0,0 +1,161 @@
+import os
+import tensorflow as tf
+import zipfile
+import datetime
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+def dir_walk(dir):
+  '''
+  Walks through a directory and displays all the sub-directories and files within it.
+
+  Args:
+    dir: the directory to be walked through.
+  '''
+  for dirpath, dirnames, filenames in os.walk(dir):
+    print(f'There are {len(dirnames)} directories and {len(filenames)} files in {dirpath}')
+
+def load_and_prep_img(img_path:str,
+                      img_size:int = 224,
+                      rescale:bool = True):
+  '''
+  Reads in an image and turns it into a tensor.
+
+  Args:
+    img_path (str): the path of the image.
+    img_size (int): the size which the image will be reshaped into (default = 224).
+    rescale (bool): whether the image needs to be normalized or not.
+  Returns:
+    The image as a normalized tensor.
+  '''
+  # Reading and decoding the image into a tensor
+  img = tf.io.read_file(img_path)
+  img = tf.image.decode_image(img)
+  # Resiszing the image into a default image size
+  img = tf.image.resize(img, [img_size, img_size])
+  # Normalizing the image
+  if rescale:
+    img = img/255.
+  else:
+    img
+  return img
+
+def plot_class_report_and_confmat(y_true, y_pred, class_names, savefig = False):
+  '''
+  Plots the classification report and the confusion matrix of a TensorFlow model.
+
+  Args:
+    y_true: the true labels of the data.
+    y_pred: the predicted labels of the data obtained from a model.
+    class_names: the names of the classes in the classificaiton problem.
+    savefig: when set to "True" saves the confusion matrix as a .png figure.
+  '''
+  # Printing the classification report
+  print(classification_report(y_true, y_pred, target_names = class_names))
+
+  # Plotting the confusion matrix
+  cm = confusion_matrix(y_true, y_pred)
+  confmat = ConfusionMatrixDisplay(cm, display_labels = class_names)
+  confmat.plot()
+
+  # Saving the confusion matrix as a figure
+  if savefig == True:
+    confmat.savefig("confusion_matrix.png")
+
+def unzip_data(path:str):
+  '''
+  Unzips a .rar file into the current working directory.
+
+  Args:
+    path (str): the path of the file to be unzipped.
+  '''
+  with zipfile.ZipFile(path) as zipref:
+    zipref.extractall()
+
+def pred_and_plot(model, filename, class_names):
+  '''
+  Predicts the class of an image using a TensorFlow model and plots it along with
+  its predicted class.
+
+  Args:
+    model: the trained TensorFlow model that will make the prediction.
+    filename: the path of the image that needs to be predicted on.
+    class_names: the names of the classes the model was trained on.
+  '''
+  # Turning the image into a tensor and adding the batch dimension to it
+  img = load_and_prep_img(filename)
+  img = tf.expand_dims(img, axis = 0)
+
+  # Making a prediction on the image using the model
+  pred_prob = model.predict(img)
+
+  # Checking if the classification is binary or multi-class and applying the appropriate functions
+  if len(pred_prob[0]) > 1:
+    pred_class = tf.argmax(pred_prob)
+    class_name = class_names[pred_class]
+  else:
+    pred_class = tf.round(pred_prob)
+    class_name = class_names[int(pred_class.numpy())]
+  
+  # Plotting the image and its predicted class
+  plt.imshow(img)
+  plt.title(f'Predicted class: {class_name}')
+  plt.axis(False)
+
+def plot_curves(history):
+  '''
+  Plots the training and validation loss and accuracy curves of a trained model.
+
+  Args:
+    history: the history of a TensorFlow model.
+  '''
+  # Getting the range of epochs for x-axis
+  epochs = range(len(history.history['loss']))
+
+  # Getting the loss and accuracy curves for the y-axis
+  loss = history.history['loss']
+  acc = history.history['accuracy']
+  val_loss = history.history['val_loss']
+  val_acc = history.history['val_accuracy']
+
+  # Plotting the loss curves
+  plt.figure()
+  plt.plot(epochs, loss, label = 'Training Loss')
+  plt.plot(epochs, val_loss, label = 'Validation Loss')
+  plt.legend()
+  plt.xlabel('Epochs')
+  plt.ylabel('Loss')
+  plt.title('Loss Curves')
+
+  # Plotting the accuracy curves
+  plt.figure()
+  plt.plot(epochs, acc, label = 'Training Accuracy')
+  plt.plot(epochs, val_acc, label = 'Validation Accuracy')
+  plt.legend()
+  plt.xlabel('Epochs')
+  plt.ylabel('Accuracy')
+  plt.title('Accuracy Curves');
+
+def create_tf_callback(dir_name:str, experiment_name:str):
+  '''
+  Creates a TensorBoard callback in the "dir_name/experiment_name/current_datetime/"
+  directory.
+
+  Args:
+    dir_name: target directory to store TensorBoard log files
+    experiment_name: name of experiment directory (e.g. efficientnet_model_1)
+  
+  Returns:
+    TensorBoard callback
+  '''
+  # Getting the current date and time
+  current_datetime = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+
+  # Creating the directory where the TensorBoard log will be saved
+  log_dir = dir_name + "/" + experiment_name + "/" + current_datetime
+
+  # Creating the callback
+  callback = tf.callbacks.TensorBoard(log_dir = log_dir)
+  print(f"Saving TensorBoard log files to: {log_dir}")
+
+  return callback
